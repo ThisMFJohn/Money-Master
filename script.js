b590f3ac04903cbb7175d53153a18db079779ce8
ThisMFJohn <zxreefhaiqal@gmail.com>
2025-03-25 09:48:37 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+// Function to get the amount from URL
+function getQueryParam(param) {
+  var urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(param);
+}
+
+// Get the amount entered by the user
+var amount = parseFloat(getQueryParam('amount')) || 1000;  // Default value if not provided
+
+// Calculate percentages
+var saving = amount * 0.30;  // 30% saving
+var spending = amount * 0.50;  // 50% spending
+var investing = amount * 0.20;  // 20% investing
+
+// Update chart with new data
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'pie',
+  data: {
+      labels: ['Saving', 'Spending', 'Investing'],
+      datasets: [{
+          label: 'Financial Breakdown',
+          data: [saving, spending, investing],
+          backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+          borderColor: '#fff',
+          borderWidth: 1
+      }]
+  },
+  options: {
+      responsive: true,
+      plugins: {
+          legend: {
+              position: 'top',
+          }
+      }
+  }
+});
